1cb7ed74c1fc9f9cb34fd13971849b93a846f9d3
Ørjan Polden <oerjanbpolden@gmail.com>
2022-04-24 09:21:12 +0200

    add new todo box and layouting

--- src/components/pages/TodoOverview/TodoOverviewPage.tsx
+++ src/components/pages/TodoOverview/TodoOverviewPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTodoStore } from '@/store/TodoStore';
-import { TodoList, TodoListItem } from '@/components/shared/TodoList';
+import { TodoList, TodoListItem } from '@/components/pages/TodoOverview/core/TodoList';
+import { AddNewTodo } from './core/AddNewTodo/AddNewTodo';
 
 const Container = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+  height: calc(100vh - 48px);
   padding: 32px;
+  box-sizing: border-box;
 `;
 
 
@@ -20,6 +25,7 @@ export const TodoOverviewPage = () => {
           <TodoListItem item={todo} />
         ))}
       </TodoList>
+      <AddNewTodo />
     </Container>
   );
 };
---
+++ src/components/pages/TodoOverview/core/AddNewTodo/AddNewTodo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #FFF;
+  color: #222;
+  padding: 32px;
+`;
+
+export const AddNewTodo = () => (
+  <Container>
+    Hello World
+  </Container>
+);
