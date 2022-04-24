71d9c1d8acfd257f220326dba33dc01870ca1c59
Ørjan Polden <oerjanbpolden@gmail.com>
2022-04-24 07:21:14 +0200

    add routing

---
+++ src/components/AppRouting/AppRouting.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { TodoOverviewPage } from '../pages/TodoOverview/TodoOverviewPage';
+
+/**
+ * Routing component. Routes in the application propagate from this root file, which
+ * is kept here separate from the App just to keep things tidy.
+ */
+export const AppRouting = () => (
+  <Routes>
+    <Route path="/*" element={<TodoOverviewPage />} />
+  </Routes>
+);
---
+++ src/components/pages/TodoOverview/TodoOverviewPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const TodoOverviewPage = () => {
+  const hello = 'world';
+
+  return (
+    <div>
+      <div>Hello World</div>
+      <div>Todo Overview Page</div>
+    </div>
+  );
+};
