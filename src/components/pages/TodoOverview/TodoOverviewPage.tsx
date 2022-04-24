6bee30998870fc7b269d2688fc0f8168df7a4b1a
Ørjan Polden <oerjanbpolden@gmail.com>
2022-04-24 12:19:07 +0200

    clamp page size

--- src/components/pages/TodoOverview/TodoOverviewPage.tsx
+++ src/components/pages/TodoOverview/TodoOverviewPage.tsx
@@ -7,6 +7,9 @@ import { AddNewTodo } from './core/AddNewTodo/AddNewTodo';
 const Container = styled.div`
   display: grid;
   grid-template-rows: 1fr auto;
+  margin: auto;
+  width: 100%;
+  max-width: 800px;
   height: calc(100vh - 48px);
   padding: 32px;
   box-sizing: border-box;
