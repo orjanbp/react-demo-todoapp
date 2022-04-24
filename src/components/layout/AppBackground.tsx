61233b5f60f0c281353872b7f55a5d5dcba4b3a7
Ørjan Polden <oerjanbpolden@gmail.com>
2022-04-24 14:00:49 +0200

    add a background

--- src/components/layout/AppBackground.tsx
+++ src/components/layout/AppBackground.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const AppBackground = styled.div`
-  background-color: #282c34;
+  background: linear-gradient(180deg,rgba(98,116,231,1) 30%,rgba(135,82,163,1) 120%);
   min-height: 100vh;
   display: flex;
   flex-direction: column;
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -6,9 +6,9 @@ export const defaultTheme: DefaultTheme = {
     background80: '#f5f5f5',
     background60: '#e0e0e0',
     background40: '#bdbdbd',
-    primary100: '#FD7D8C',
-    primary80: '#FC5E70',
-    primary60: '#CA4B5A',
+    primary100: '#fb9644',
+    primary80: '#fa811e',
+    primary60: '#e56a05',
     border: '#bdbdbd',
   }
 }
